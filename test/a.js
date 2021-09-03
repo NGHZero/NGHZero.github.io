57cd747d7fa643ba4bc6857e0ab1c3def4ffcb94
NGHZero <88583987+NGHZero@users.noreply.github.com>
2021-09-03 08:06:42 +0700

    b

---
+++ test/a.js
@@ -0,0 +1,19 @@
+// cau1:
+function findEven(number){
+if(number>4 && number<20){
+for(let i =0; i<number; i++){
+    if(i&2 ==0)
+    { console.log(i);}
+   
+}
+
+}else{
+    console.log(" số bạn nhập ko hợp lệ");
+}
+    
+}
+
+
+
+
+
